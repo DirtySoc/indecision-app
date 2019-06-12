5ae6f4d08b46a3d3900c4853ed5ba1b46f9de66b
DirtySoc <derek.willingham@outlook.com>
2019-06-12 17:29:45 -0400

    updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,26 @@
 "use strict";
 
-console.log('app.js is running!'); // JSX - JavaScript XML
+console.log('app.js is running!');
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Helping you decide!',
+  options: ['One', 'Two'] // JSX - JavaScript XML
 
-var template = React.createElement("div", null, React.createElement("h1", null, "Indecision App"), React.createElement("p", null, "This is some info"), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Two")));
-var templateTwo = React.createElement("div", null, React.createElement("h1", null, "Derek Willingham"), React.createElement("p", null, "Age: 23"), React.createElement("p", null, "Location: Atlanta"));
+};
+var template = React.createElement("div", null, React.createElement("h1", null, app.title), app.subtitle && React.createElement("p", null, app.subtitle), app.options.length > 0 ? React.createElement("p", null, "Here are your options!") : React.createElement("p", null, "NO OPTIONS"), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Two")));
+var user = {
+  // name: 'Derek',
+  age: 19,
+  location: 'Woodstock'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement("p", null, "Location: ", location);
+  }
+}
+
+var templateTwo = React.createElement("div", null, React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
 var appRoot = document.getElementById('app'); // ReactDOM.render(template, appRoot);
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,16 @@
-console.log('app.js is running!')
+console.log('app.js is running!');
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Helping you decide!',
+    options: ['One', 'Two']
+}
 
 // JSX - JavaScript XML
 var template = (
     <div>
-        <h1>Indecision App</h1>
-        <p>This is some info</p>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {app.options.length > 0 ? <p>Here are your options!</p>:<p>NO OPTIONS</p> }
         <ol>
             <li>Item One</li>
             <li>Item Two</li>
@@ -12,15 +18,27 @@ var template = (
     </div>
 );
 
+var user = {
+    // name: 'Derek',
+    age: 19,
+    location: 'Woodstock'
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>
+    }
+}
+
 var templateTwo = (
     <div>
-        <h1>Derek Willingham</h1>
-        <p>Age: 23</p>
-        <p>Location: Atlanta</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
 var appRoot = document.getElementById('app')
 
 // ReactDOM.render(template, appRoot);
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
